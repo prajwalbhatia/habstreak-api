35d92b03160e2734b892c6e9c52e036216921e2c
Prajwal <prajwal6bhatia@gmail.com>
2022-05-22 01:12:56 +0530

    FIX : Same date bug fixed

--- models/streakDetail.js
+++ models/streakDetail.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const streakDetailSchema = mongoose.Schema({
   userId: { type: String, require: true },
-  date: { type: String, default: Date.now() },
+  date: { type: String, require : true},
   description : {type : String , default : ''},
   reward : {type : Boolean , default : false},
   rewards: [{ type: String }],
